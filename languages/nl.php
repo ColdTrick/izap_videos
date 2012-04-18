a0efd00f80ea0c4abb0f53549d1140004fa217d7
rickterheide <rick@coldtrick.com>
2012-04-18 16:48:45 +0200

    Uploaded for archiving purposes

---
+++ languages/nl.php
@@ -0,0 +1,90 @@
+<?php
+	/**
+	 * iZAP izap_videos
+	 * 
+	 * @package youtube, vimeo, veoh and onserver uploading
+	 * @license GNU Public License version 3
+	 * @author iZAP Team "<support@izap.in>"
+	 * @link http://www.izap.in/
+	 * @version 1.5-0.6
+	 */
+	 
+	 /**
+	* Dutch translation.
+	* 
+	* @package dutch_translation
+	* @author ColdTrick IT Solutions
+	* @copyright Coldtrick IT Solutions 2009
+	* @link http://www.coldtrick.com/
+	*/
+	$dutch = array (
+		
+		'izap_videos' => "iZAP Video's",
+		'videos' => "Video's",
+		'izap_videos:videos' => "Mijn video's",
+		'izap_videos:add' => "Nieuwe video",
+		'izap_videos:addgroup' => "Voeg nieuwe groeps video toe",
+		'izap_videos:user' => "%s's video",
+		'izap_videos:addurl' => "Geef de URL van de video op. (Ondersteunt Youtube, Vimeo, Veoh)",
+		'izap_videos:title' => "Titel",
+		'izap_videos:getvideo' => "Plaats video",
+		'izap_videos:access' => "Toegang",
+		'izap_videos:tags' => "Video tags",
+		'izap_videos:blank' => "Geef een geldige URL op",
+		'izap_videos:blanktitle' => "Geef een titel op",
+		'izap_videos:saveerror' => "Excuses, video kan niet worden opgeslagen",
+		'izap_videos:saved' => "Je video is opgeslagen",
+		'izap_videos:everyone' => "Alle video's",
+		'izap_videos:time' => "Toegevoegd op %s",
+		'izap_videos:remove' => "Wil je deze video verwijderen?",
+		'izap_videos:deleted' => "De video is succesvol verwijderd",
+		'izap_videos:notdeleted' => "De video kon niet worden verwijderd",
+		'izap_videos:wrongid' => "Excuses, de video is verwijderd of een verkeerde ID",
+		'izap_videos:frnd' => "Vrienden video's",
+		'izap_videos:userfrnd' => "%s's vrienden Video's",
+		'izap_videos:all' => "Alle video's",
+		'izap_videos:river:added' => "%s plaatste een nieuwe ",
+		'izap_videos:river:video' => "Video",
+		'izap_videos:river:annotate' => " een reactie op ",
+		'izap_videos:widget' => "Een lijst van de laatste video's van de gebruiker",
+		'item:object:izap_videos' => "Video's",
+		'izap_videos:numbertodisplay' => "Aantal video's om weer te geven.",
+		'izap_videos:play' => "Start de gekozen video",
+		'izap_videos:play:widget' => "Deze widget zal je gekozen video afspelen. Een voor een",
+		'izap_videos:chosevideo' => "Kies een video op af te spelen",
+		'izap_videos:notfound' => "Excuses, gebruiker heeft geen publieke video's.",
+		'izap_videos:error' => "Fout tijden het ophalen van de externe video. Of een foute URL of video niet gevonden.",
+		'izap_videos:addtoyour' => "Voeg deze video toe aan Mijn video's.",
+		'izap_videos:condition' => "* Bovenstaande titel, tags en omschrijving overschrijven de originele informatie.<br>* Video bron is verplicht (URL of Upload)",
+		'izap_videos:geterror' => "Kan de video niet vinden. Site reageerd niet.",
+		'izap_videos:description' => "Omschrijving",
+		'izap_videos:edit' => "Bewerk",
+		'izap_videos:blank:title' => "Titel mag niet leeg zijn",
+		'izap_videos:save' => "Opslaan",
+		'izap_videos:missingfields' => "Verplichte velden ontbreken.",
+		'izap_videos:autoplay' => "Automatisch afspelen?",
+		'izap_videos:views' => "Aantal keer bekeken",
+		'izap_videos:tagcloud' => "Top video tags",
+		'izap_videos:top' => "Top video's",
+		'izap_videos:fopenerror' => "Vraag je Site Administrator om <b>'allow_url_fopen = On'</b> op te nemen in de php.ini.",
+		'izap_videos:errorCode' => "Fout tijdens het ophalen van de externe URL. Of een foute URL of video niet gevonden",
+		'izap_videos:errorCode101' => "Kan de video niet vinden. Site reageerd niet.",
+		'izap_videos:errorCode102' => "Deze video behoort niet tot Veoh, kopieer aub originele Veoh video.",
+		'izap_videos:latest' => "Nieuwste video's",
+		'izap_videos:groups' => "%s's video's",
+        'izap_videos:group:enablevideo' => "Schakel groep video's in",
+		'izap_videos:groupvideos' => "Groep video's",
+		'izap_videos:izapUploadOption' => "Video type:",
+		'izap_videos:upload' => "Upload video bestand (Ondersteunt 3gp, avi, mp4, flv)",
+		'izap_videos:unsupported' => "Niet ondersteunt video formaat.",
+		'izap_videos:converterror' => "Fout tijdens omzetten video.",
+		'izap_videos:izapBorderColor1' => "Geef rand kleur1 op voor de videospeler: (kleurcode zonder '#')",
+		'izap_videos:izapBorderColor2' => "Geef rand kleur2 op voor de videospeler: (kleurcode zonder '#')",
+		'izap_videos:izapBarColor' => "Geef de balk kleur op voor de videospeler: (kleurcode zonder '#')",
+		'izap_videos:river:titled' => '%s plaatste een nieuwe video genaamd %s',
+        'izap_videos:latestvideos' => "Nieuwste video's",
+		
+	);
+	
+	add_translation("nl", $dutch);
+?>
